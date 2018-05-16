08501714ecad9dee1bb0f500aacc037ddf109197
kumarbhrgv <kumarbhrgv201@gmail.com>
2018-05-16 02:36:38 -0600

    change port for heroku

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ var port = 8000,
 	express = require('express'),
 	app = express();
 app.use('/', express.static(__dirname));
-app.listen(port);
+app.listen(process.env.PORT || port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 console.log('Now serving http://localhost:' + port + '/index.html');
